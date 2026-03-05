8ffb9d76ac996c3214f519a6dd8065ce82fce2e4
Ragul M <ragulm12a1@gmail.com>
2026-03-05 15:11:13 +0530

    Fixed CORS issue

--- backend/src/main/java/com/blog/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/blog/backend/config/SecurityConfig.java
@@ -85,7 +85,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "https://blog-platform-beta-rose.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
